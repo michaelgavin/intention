7e9ebfc9136e0cde19ffa7a8ebb0ea3ff4fe3899
Michael Gavin <michael.a.gavin@gmail.com>
2018-08-28 15:24:11 -0400

    latest tutorial commit

--- R/set_threshold.R
+++ R/set_threshold.R
@@ -1,26 +1,26 @@
 #' Measure normalization threshold
 #' 
 #' @export
-set_threshold = function(avgs, devs, mu = NULL) {
+set_threshold = function(avgs, devs, mu = NA) {
   x = 1:length(avgs)
   fita = lm(avgs~log(x))
   anew = predict(fita,newdata=list(x=x),interval="confidence")
   fitd = lm(devs~log(x))
   dnew = predict(fitd,newdata=list(x=x),interval="confidence")
   delta = anew[,1]
-  psi = dnew[,1]
-  if (any(mu) == F) {
-    mu = (1-delta[1]) / psi[1]
+  phi = dnew[,1]
+  if (is.na(mu)) {
+    mu = (1-delta[1]) / phi[1]
   }
-  theta = delta + mu * psi
+  theta = delta + mu * phi
   res = list()
   res$mu = mu
   res$avg_alpha = coefficients(fita)[2]
   res$avg_beta = coefficients(fita)[1]
-  res$psi_alpha = coefficients(fitd)[2]
-  res$psi_beta = coefficients(fitd)[1]
+  res$phi_alpha = coefficients(fitd)[2]
+  res$phi_beta = coefficients(fitd)[1]
   res$delta = delta
-  res$psi = psi
+  res$phi = phi
   res$theta = theta
   return(res)
 }
